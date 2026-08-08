341821ea5415f46f265cab59dee8875ee82319e5
Emiliano Ledesma <emilianoledesmaledesma@outlook.com>
2026-08-08 15:53:57 -0600

    fix(mobile): actualizar LAN IP hardcodeada para pruebas en dispositivo real

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -6,7 +6,7 @@ export default {
     orientation: 'portrait',
     plugins: ['expo-secure-store'],
     extra: {
-      apiUrl: process.env.API_URL || 'http://10.16.72.248:8010',
+      apiUrl: process.env.API_URL || 'http://192.168.100.31:8010',
     },
   },
 };
